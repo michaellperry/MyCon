315a35ea161c2df1199906aaf26a5251bbd34c87
Michael L Perry <mperry@adventuresinsoftware.com>
2011-06-27 23:27:49 -0500

    Added tilt effect to session items.

--- WP7/FacetedWorlds.MyCon/Views/SessionUserControl.cs
+++ WP7/FacetedWorlds.MyCon/Views/SessionUserControl.cs
@@ -5,13 +5,34 @@ using System.Windows.Input;
 using FacetedWorlds.MyCon.ViewModels;
 using Microsoft.Phone.Controls;
 using UpdateControls.XAML;
+using System.Windows.Media;
 
 namespace FacetedWorlds.MyCon.Views
 {
     public class SessionUserControl : UserControl
     {
+        private const double RadiansToDegrees = 180.0 / Math.PI;
+
+        private bool _mouseDown = false;
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            ShowTilt(e.GetPosition(this));
+            _mouseDown = true;
+            base.OnMouseLeftButtonDown(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (_mouseDown)
+                ShowTilt(e.GetPosition(this));
+            base.OnMouseMove(e);
+        }
+
         protected override void OnManipulationCompleted(ManipulationCompletedEventArgs e)
         {
+            SetProjection(0.0, 0.0);
+            _mouseDown = false;
             if (e.TotalManipulation.Translation.X == 0 && e.TotalManipulation.Translation.Y == 0)
             {
                 SessionViewModelBase viewModel = ForView.Unwrap<SessionViewModelBase>(DataContext);
@@ -25,6 +46,34 @@ namespace FacetedWorlds.MyCon.Views
             }
 
             base.OnManipulationCompleted(e);
+        }
+
+        private void ShowTilt(Point point)
+        {
+            double halfWidth = ActualWidth / 2;
+            double halfHeight = ActualHeight / 2;
+
+            double xAngle = 0.0;
+            if (point.Y > 0.0 && point.Y < ActualHeight)
+                xAngle = Math.Asin((point.Y - halfHeight) / halfHeight) * RadiansToDegrees / 4.0;
+
+            double yAngle = 0.0;
+            if (point.X > 0.0 && point.X < ActualWidth)
+                yAngle = Math.Asin(-(point.X - halfWidth) / halfWidth) * RadiansToDegrees / 4.0;
+
+            SetProjection(xAngle, yAngle);
+        }
+
+        private void SetProjection(double xAngle, double yAngle)
+        {
+            PlaneProjection projection = Projection as PlaneProjection;
+            if (projection == null)
+            {
+                projection = new PlaneProjection();
+                Projection = projection;
+            }
+            projection.RotationX = xAngle;
+            projection.RotationY = yAngle;
         }
     }
 }
