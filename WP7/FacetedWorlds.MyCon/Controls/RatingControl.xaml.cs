6a8ffab9bd7eef9f927491013b71aacd3a1fd152
Michael L Perry <mperry@adventuresinsoftware.com>
2011-07-02 23:17:35 -0500

    Rate on tap, not drag.

--- WP7/FacetedWorlds.MyCon/Controls/RatingControl.xaml.cs
+++ WP7/FacetedWorlds.MyCon/Controls/RatingControl.xaml.cs
@@ -8,7 +8,6 @@ namespace FacetedWorlds.MyCon.Controls
     public partial class RatingControl : UserControl
     {
         private const int StarSize = 78;
-        private const int VerticalScrollStart = 10;
 
         public static DependencyProperty ValueProperty = DependencyProperty.Register(
             "Value",
@@ -17,45 +16,17 @@ namespace FacetedWorlds.MyCon.Controls
             new PropertyMetadata((control, args) =>
                 ((RatingControl)control).ValueChanged((int)args.NewValue)));
 
-        private int _oldValue = 0;
-        private double _startY = 0.0;
-        private bool _editing = false;
-
         public RatingControl()
         {
             InitializeComponent();
         }
 
-        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        protected override void OnManipulationCompleted(ManipulationCompletedEventArgs e)
         {
-            _editing = true;
-            _oldValue = Value;
-            Point position = e.GetPosition(this);
-            _startY = position.Y;
-            HandleMouseEvent(position);
-            base.OnMouseLeftButtonDown(e);
-        }
+            if (e.TotalManipulation.Translation.X == 0.0 && e.TotalManipulation.Translation.Y == 0.0)
+                HandleMouseEvent(e.ManipulationOrigin);
 
-        protected override void OnMouseMove(MouseEventArgs e)
-        {
-            if (_editing)
-            {
-                Point position = e.GetPosition(this);
-                if (Math.Abs(position.Y - _startY) > VerticalScrollStart)
-                {
-                    _editing = false;
-                    Value = _oldValue;
-                }
-                else
-                    HandleMouseEvent(position);
-            }
-            base.OnMouseMove(e);
-        }
-
-        protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
-        {
-            _editing = false;
-            base.OnMouseLeftButtonUp(e);
+            base.OnManipulationCompleted(e);
         }
 
         private void HandleMouseEvent(Point position)
