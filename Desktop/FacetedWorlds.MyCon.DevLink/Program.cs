37d7f164f827b9469015f8060c0366ce7827740a
michaellperry <mperry@adventuresinsoftware.com>
2011-07-07 13:03:05 -0500

    Generate speakers.

--- Desktop/FacetedWorlds.MyCon.DevLink/Program.cs
+++ Desktop/FacetedWorlds.MyCon.DevLink/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using FacetedWorlds.MyCon.DevLink.DevLinkServiceReference;
 using System.Threading;
+using System.Text.RegularExpressions;
 
 namespace FacetedWorlds.MyCon.DevLink
 {
@@ -16,15 +17,14 @@ namespace FacetedWorlds.MyCon.DevLink
             _entities = new devlink2011Entities(new Uri("http://devlink.us/odata.svc/", UriKind.Absolute));
 
             GenerateTimeSlots();
+            GenerateSpeakers();
         }
 
         private static void GenerateTimeSlots()
         {
             TimeZoneInfo est = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
 
-            var times = _entities.timeslots;
-
-            foreach (var time in times)
+            foreach (var time in _entities.timeslots)
             {
                 DateTime localStart = TimeZoneInfo.ConvertTimeFromUtc(time.start, est);
                 Console.WriteLine(String.Format("_timeById[\"{1}\"] = conference.GetTime(DateTime.Parse(\"{0}\"));", localStart, time.timeslotid));
@@ -33,6 +33,27 @@ namespace FacetedWorlds.MyCon.DevLink
                     Console.WriteLine(String.Format("conference.NewGeneralSessionPlace(\"gen_{0}\", \"{1}\", _timeById[\"{2}\"], string.Empty);", time.timeslotid, time.description, time.timeslotid));
                 }
             }
+        }
+
+        private static void GenerateSpeakers()
+        {
+            foreach (var speaker in _entities.speakers)
+            {
+                string speakerName = speaker.firstname + " " + speaker.lastname;
+                string contact = "@" + speaker.twitter;
+                string speakerBio = StringEncode(speaker.bio);
+                string speakerImageUrl = speaker.imageurl;
+
+                Console.WriteLine(String.Format("_speakerById[\"{4}\"] = conference.NewSpeaker(\"{0}\", \"{1}\", \"{2}\", \"{3}\");", speakerName, contact, speakerBio, speakerImageUrl, speaker.speakerid));
+            }
+        }
+
+        private static string StringEncode(string value)
+        {
+            return value
+                .Replace("\\", "\\\\")
+                .Replace("\n", "\\n")
+                .Replace("\"", "\\\"");
         }
     }
 }
