430a4ca06bfc53261d8f6b17fa5d48c97a67ddc5
Michael L Perry <mperry@adventuresinsoftware.com>
2011-07-29 21:04:28 -0500

    Dispatch an image download every quarter second.

--- WP7/FacetedWorlds.MyCon/ImageUtilities/RequestQueue.cs
+++ WP7/FacetedWorlds.MyCon/ImageUtilities/RequestQueue.cs
@@ -2,38 +2,61 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace FacetedWorlds.MyCon.ImageUtilities
 {
     public class RequestQueue
     {
         private Queue<Action<Action>> _requests = new Queue<Action<Action>>();
+        private DispatcherTimer _timer = new DispatcherTimer();
+        private bool _running = false;
+
+        public RequestQueue()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(250);
+            _timer.Tick += new EventHandler(TimerTick);
+        }
 
         public void QueueRequest(Action<Action> request)
         {
-            bool wasEmpty;
             lock (this)
             {
-                wasEmpty = _requests.Count == 0;
+                bool wasEmpty = _requests.Count == 0;
                 _requests.Enqueue(request);
-            }
-            if (wasEmpty)
-            {
-                Deployment.Current.Dispatcher.BeginInvoke(() => request(InvokeNextRequest));
+                if (wasEmpty)
+                {
+                    _timer.Start();
+                }
             }
         }
 
         private void InvokeNextRequest()
         {
-            Action<Action> nextRequest = null;
             lock (this)
             {
-                if (_requests.Count > 0)
-                    nextRequest = _requests.Dequeue();
+                _running = false;
             }
-            if (nextRequest != null)
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            lock (this)
             {
-                Deployment.Current.Dispatcher.BeginInvoke(() => nextRequest(InvokeNextRequest));
+                if (!_running)
+                {
+                    if (_requests.Count > 0)
+                    {
+                        _running = true;
+                        var request = _requests.Dequeue();
+                        request(InvokeNextRequest);
+                    }
+                    else
+                    {
+                        _timer.Stop();
+                    }
+                }
             }
         }
     }
