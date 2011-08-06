33c7ac1e165466ed0bd55976c52aa0077f7ce12e
Michael L Perry <mperry@adventuresinsoftware.com>
2011-08-06 10:11:19 -0500

    Scroll when the user drags close to the edge.

--- Silverlight4/FacetedWorlds.MyCon/Views/ScheduleView.xaml.cs
+++ Silverlight4/FacetedWorlds.MyCon/Views/ScheduleView.xaml.cs
@@ -1,28 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using SL_Drag_Drop_BaseClasses;
-using System.Diagnostics;
-using UpdateControls.XAML;
 using FacetedWorlds.MyCon.ViewModels;
+using SL_Drag_Drop_BaseClasses;
+using UpdateControls.XAML;
+using System.Windows.Threading;
 
 namespace FacetedWorlds.MyCon.Views
 {
     public partial class ScheduleView : UserControl
     {
+        private const double ScrollBy = 50.0;
+        private const double ScrollEdge = 8.0;
+
+        private DispatcherTimer _timer = new DispatcherTimer();
+        private Point _position;
+
         public ScheduleView()
         {
             InitializeComponent();
 
             InitialValues.ContainingLayoutPanel = this.LayoutRoot;
+
+            _timer.Interval = TimeSpan.FromSeconds(0.25);
+            _timer.Tick += Timer_Tick;
         }
 
         private void DropTarget_DragSourceDropped(object sender, DropEventArgs args)
@@ -38,6 +40,39 @@ namespace FacetedWorlds.MyCon.Views
                     Debug.WriteLine(String.Format("{0} dropped on {1}.", source, target));
                 }
             }
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+        }
+
+        void Timer_Tick(object sender, EventArgs e)
+        {
+            if (_position.X < ScrollEdge)
+                Scroller.ScrollToHorizontalOffset(Scroller.HorizontalOffset - ScrollBy);
+            else if (_position.X > Scroller.ViewportWidth - ScrollEdge)
+                Scroller.ScrollToHorizontalOffset(Scroller.HorizontalOffset + ScrollBy);
+            if (_position.Y < ScrollEdge)
+                Scroller.ScrollToVerticalOffset(Scroller.VerticalOffset - ScrollBy);
+            else if (_position.Y > Scroller.ViewportHeight - ScrollEdge)
+                Scroller.ScrollToVerticalOffset(Scroller.VerticalOffset + ScrollBy);
+        }
+
+        private void DragSource_DragStarted(object sender, SL_Drag_Drop_BaseClasses.DragEventArgs args)
+        {
+            _timer.Start();
+            _position = args.MouseEventArgs.GetPosition(Scroller);
+        }
+
+        private void DragSource_DragFinished(object sender, SL_Drag_Drop_BaseClasses.DragEventArgs args)
+        {
+            _timer.Stop();
+        }
+
+        private void DragSource_DragMoved(object sender, SL_Drag_Drop_BaseClasses.DragEventArgs args)
+        {
+            _position = args.MouseEventArgs.GetPosition(Scroller);
         }
     }
 }
