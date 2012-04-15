f853092932581f253e6264d5014be030c047ad20
Michael L Perry <mperry@adventuresinsoftware.com>
2012-04-15 14:07:35 -0500

    Added sponsors page.

---
+++ Web/FacetedWorlds.MyCon.Web/Controllers/SponsorsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FacetedWorlds.MyCon.Web.ViewModels;
+
+namespace FacetedWorlds.MyCon.Web.Controllers
+{
+    public class SponsorsController : Controller
+    {
+        //
+        // GET: /Sponsors/
+
+        public ActionResult Index()
+        {
+            return View(new SponsorsViewModel(MvcApplication.SynchronizationService.Conference));
+        }
+    }
+}
---
+++ Web/FacetedWorlds.MyCon.Web/ViewModels/SponsorsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FacetedWorlds.MyCon.Model;
+
+namespace FacetedWorlds.MyCon.Web.ViewModels
+{
+    public class SponsorsViewModel
+    {
+        private readonly Conference _conference;
+
+        public SponsorsViewModel(Conference conference)
+        {
+            _conference = conference;
+        }
+
+        public string ConferenceName
+        {
+            get { return _conference.Name; }
+        }
+    }
+}
